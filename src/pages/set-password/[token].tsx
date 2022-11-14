b8af6e22d4168c13d824e9adef6cffdff0d61a0b
Mateusz Drzał <mdrzal1886@gmail.com>
2022-11-14 19:10:55 +0100

    Create logic to set-password/[token] page

--- src/pages/set-password/[token].tsx
+++ src/pages/set-password/[token].tsx
@@ -1,9 +1,84 @@
-import { useRouter } from 'next/router';
+import type {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  NextPage
+} from 'next';
+import jwt from 'jwt-simple';
 
-const SetPassword = () => {
-  const { query } = useRouter();
-  console.log(query);
-  return <div>Token</div>;
+import { connectToDatabase } from 'src/api/db';
+
+interface IProps {
+  email?: string;
+  isTokenExist: boolean;
+  token: string;
+}
+
+const SetPassword: NextPage<IProps> = ({ email, isTokenExist, token }) => {
+  if (!isTokenExist) {
+    return <div>Invalid token</div>;
+  }
+
+  if (email) {
+    return <div>User with email: {email} was activated</div>;
+  }
+
+  return (
+    <form>
+      <input type="password" />
+      <input type="password" />
+      <button>Set Password</button>
+    </form>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<IProps> = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const { token = '' } = ctx.params || {};
+
+  const tokenString = token.toString();
+
+  const client = await connectToDatabase();
+
+  const users = client.db().collection('users');
+
+  const usersWithoutActivation = client
+    .db()
+    .collection('users-without-activation');
+
+  const userToActivate = await usersWithoutActivation.findOne({
+    token: tokenString
+  });
+
+  if (!userToActivate) {
+    return {
+      props: {
+        isTokenExist: false,
+        token: tokenString
+      }
+    };
+  }
+
+  const email: string = jwt.decode(tokenString, process.env.JWT_SECRET!);
+
+  const user = await users.findOne({ email });
+
+  if (user) {
+    return {
+      props: {
+        email,
+        isTokenExist: true,
+        token: tokenString
+      }
+    };
+  }
+
+  return {
+    props: {
+      isTokenExist: true,
+      token: tokenString
+    }
+  };
 };
 
 export default SetPassword;
