ab85129e7a14e5233617bd5719b56d90105f1c81
Mateusz Drzał <mdrzal1886@gmail.com>
2022-12-09 19:11:08 +0100

    Add style to theme switch

--- src/components/theme-switch/ThemeSwitch.tsx
+++ src/components/theme-switch/ThemeSwitch.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 
+import Sun from 'src/assets/svg/sun.svg';
+import Moon from 'src/assets/svg/moon.svg';
+import styles from './themeSwitch.module.scss';
+
 const ThemeSwitch = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
@@ -23,11 +27,30 @@ const ThemeSwitch = () => {
     return null;
   }
 
-  if (currentTheme === 'dark') {
-    return <button onClick={toggleTheme}>Light</button>;
-  }
-
-  return <button onClick={toggleTheme}>Dark</button>;
+  return (
+    <div
+      className={`${styles.switch} ${
+        currentTheme === 'dark' ? styles.switchDark : styles.switchLight
+      }`}
+      onClick={(e) => {
+        e.currentTarget.blur();
+        toggleTheme();
+      }}
+      onKeyDown={(e) => {
+        if (e.code !== 'Enter') return;
+        toggleTheme();
+      }}
+      tabIndex={1}
+    >
+      <div
+        className={`${styles.dot} ${
+          currentTheme === 'dark' ? styles.dotDark : styles.dotLight
+        }`}
+      >
+        {currentTheme === 'dark' ? <Sun /> : <Moon />}
+      </div>
+    </div>
+  );
 };
 
 export default ThemeSwitch;
