b3d3d1546e77018301fb3d020c687ed76a27ad25
Mateusz Drzał <mdrzal1886@gmail.com>
2022-12-20 17:59:46 +0100

    Add log in, sign up and theme switch to menu list

--- src/components/layout/header/nav/mobile/menu-list/MenuList.tsx
+++ src/components/layout/header/nav/mobile/menu-list/MenuList.tsx
@@ -1,10 +1,14 @@
 import { FC, useEffect, useRef } from 'react';
 import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
 import gsap from 'gsap';
 
 import styles from './menuList.module.scss';
 import useFocusTrap from 'src/hooks/useFocusTrap';
 import { routeLinks } from 'src/dictionary/routeLinks';
+import ThemeSwitch from 'src/components/theme-switch/ThemeSwitch';
+import useModalContext from 'src/context/ModalContext';
+import { ModalsContent } from 'src/utils/getModalsContent';
 
 interface IProps {
   isMenuOpen: boolean;
@@ -15,15 +19,28 @@ interface IProps {
 const MenuList: FC<IProps> = ({ isMenuOpen, hamburgerButton, closeMenu }) => {
   const menuRef = useRef<HTMLDivElement>(null);
 
+  const { openModal, passModalContent } = useModalContext();
+
+  const { status } = useSession();
+
+  const logOut = async () => {
+    try {
+      await signOut({
+        redirect: false
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     if (!menuRef.current) return;
 
     const menuElement = menuRef.current;
     const tl = gsap.timeline();
 
-    const { keydownCallback } = useFocusTrap(menuRef.current, hamburgerButton);
-
     if (isMenuOpen) {
+      const { keydownCallback } = useFocusTrap(menuElement, hamburgerButton);
       document.body.style.overflow = 'hidden';
       document.addEventListener('keydown', keydownCallback);
 
@@ -67,6 +84,29 @@ const MenuList: FC<IProps> = ({ isMenuOpen, hamburgerButton, closeMenu }) => {
           </Link>
         </li>
       </ul>
+      {status === 'authenticated' ? (
+        <button onClick={logOut}>Log out</button>
+      ) : (
+        <button
+          onClick={() => {
+            closeMenu();
+            passModalContent(ModalsContent.LogIn);
+            openModal();
+          }}
+        >
+          Log in
+        </button>
+      )}
+      <button
+        onClick={() => {
+          closeMenu();
+          passModalContent(ModalsContent.SignUp);
+          openModal();
+        }}
+      >
+        Sign Up
+      </button>
+      <ThemeSwitch />
     </div>
   );
 };
