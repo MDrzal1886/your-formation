6420494f6cb476e7f5a73aefbf8ee028c1c9ef6d
Mateusz Drzał <mdrzal1886@gmail.com>
2022-12-22 18:04:23 +0100

    Refactor log in with react-hook-form

--- src/components/modal/content/log-in/ContentLogIn.tsx
+++ src/components/modal/content/log-in/ContentLogIn.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useRef } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { signIn } from 'next-auth/react';
 
 import useModalContext from 'src/context/ModalContext';
@@ -6,29 +6,34 @@ import useNotificationContext, {
   NotificationStatus
 } from 'src/context/NotificationContext';
 
+interface IInputs {
+  email: string;
+  password: string;
+}
+
 const ContentLogIn = () => {
-  const emailInputRef = useRef<HTMLInputElement>(null);
-  const passwordInputRef = useRef<HTMLInputElement>(null);
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<IInputs>();
 
   const { closeModal } = useModalContext();
 
   const { showNotification } = useNotificationContext();
 
-  const onLogIn = async (e: FormEvent) => {
-    e.preventDefault();
+  const onLogIn: SubmitHandler<IInputs> = async (data) => {
     try {
-      const enteredEmail = emailInputRef.current?.value || '';
-      const enteredPassword = passwordInputRef.current?.value || '';
-
       const res = await signIn('credentials', {
         redirect: false,
-        email: enteredEmail,
-        password: enteredPassword
+        email: data.email,
+        password: data.password
       });
 
-      if (res?.ok && emailInputRef.current && passwordInputRef.current) {
-        emailInputRef.current.value = '';
-        passwordInputRef.current.value = '';
+      if (res?.ok) {
+        setValue('email', '');
+        setValue('password', '');
 
         showNotification({
           title: 'You are log in',
@@ -40,9 +45,9 @@ const ContentLogIn = () => {
       }
 
       showNotification({
-        title: res?.error || 'Something went wrong',
+        title: 'Error',
         status: NotificationStatus.Error,
-        message: 'Wrong'
+        message: res?.error || 'Something went wrong'
       });
     } catch (error) {
       console.log(error);
@@ -50,15 +55,26 @@ const ContentLogIn = () => {
   };
   return (
     <div>
-      <form onSubmit={onLogIn}>
+      <form
+        onSubmit={handleSubmit((data) => onLogIn(data))}
+        noValidate
+      >
         <input
           type="email"
-          ref={emailInputRef}
+          {...register('email', {
+            required: 'This input is required',
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: 'Incorrect email'
+            }
+          })}
         />
+        {errors.email && <p>{errors.email.message}</p>}
         <input
           type="password"
-          ref={passwordInputRef}
+          {...register('password', { required: 'This input is required' })}
         />
+        {errors.password && <p>{errors.password.message}</p>}
         <button>Log in</button>
       </form>
     </div>
