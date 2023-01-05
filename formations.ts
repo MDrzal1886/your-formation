f9627ebfa9d933eda10c754bd77c3af653d38fcc
Mateusz Drzał <mdrzal1886@gmail.com>
2023-01-05 19:33:12 +0100

    Create default formations

---
+++ formations.ts
@@ -0,0 +1,503 @@
+export const fourFourTwo = [
+  {
+    num: 1,
+    smallPitch: {
+      posX: 135,
+      posY: 30
+    },
+    middlePitch: {
+      posX: 180,
+      posY: 55
+    },
+    largePitch: {
+      posX: 225,
+      posY: 80
+    }
+  },
+  {
+    num: 2,
+    smallPitch: {
+      posX: 36,
+      posY: 110
+    },
+    middlePitch: {
+      posX: 48,
+      posY: 165
+    },
+    largePitch: {
+      posX: 60,
+      posY: 220
+    }
+  },
+  {
+    num: 3,
+    smallPitch: {
+      posX: 102,
+      posY: 110
+    },
+    middlePitch: {
+      posX: 136,
+      posY: 165
+    },
+    largePitch: {
+      posX: 170,
+      posY: 220
+    }
+  },
+  {
+    num: 4,
+    smallPitch: {
+      posX: 168,
+      posY: 110
+    },
+    middlePitch: {
+      posX: 224,
+      posY: 165
+    },
+    largePitch: {
+      posX: 280,
+      posY: 220
+    }
+  },
+  {
+    num: 5,
+    smallPitch: {
+      posX: 234,
+      posY: 110
+    },
+    middlePitch: {
+      posX: 312,
+      posY: 165
+    },
+    largePitch: {
+      posX: 390,
+      posY: 220
+    }
+  },
+  {
+    num: 6,
+    smallPitch: {
+      posX: 102,
+      posY: 190
+    },
+    middlePitch: {
+      posX: 136,
+      posY: 285
+    },
+    largePitch: {
+      posX: 170,
+      posY: 350
+    }
+  },
+  {
+    num: 7,
+    smallPitch: {
+      posX: 36,
+      posY: 190
+    },
+    middlePitch: {
+      posX: 48,
+      posY: 285
+    },
+    largePitch: {
+      posX: 60,
+      posY: 350
+    }
+  },
+  {
+    num: 8,
+    smallPitch: {
+      posX: 168,
+      posY: 190
+    },
+    middlePitch: {
+      posX: 224,
+      posY: 285
+    },
+    largePitch: {
+      posX: 280,
+      posY: 350
+    }
+  },
+  {
+    num: 9,
+    smallPitch: {
+      posX: 168,
+      posY: 270
+    },
+    middlePitch: {
+      posX: 224,
+      posY: 405
+    },
+    largePitch: {
+      posX: 280,
+      posY: 480
+    }
+  },
+  {
+    num: 10,
+    smallPitch: {
+      posX: 102,
+      posY: 270
+    },
+    middlePitch: {
+      posX: 136,
+      posY: 405
+    },
+    largePitch: {
+      posX: 170,
+      posY: 480
+    }
+  },
+  {
+    num: 11,
+    smallPitch: {
+      posX: 234,
+      posY: 190
+    },
+    middlePitch: {
+      posX: 312,
+      posY: 285
+    },
+    largePitch: {
+      posX: 390,
+      posY: 350
+    }
+  }
+];
+
+export const fourThreeThree = [
+  {
+    num: 1,
+    smallPitch: {
+      posX: 135,
+      posY: 30
+    },
+    middlePitch: {
+      posX: 180,
+      posY: 55
+    },
+    largePitch: {
+      posX: 225,
+      posY: 80
+    }
+  },
+  {
+    num: 2,
+    smallPitch: {
+      posX: 36,
+      posY: 110
+    },
+    middlePitch: {
+      posX: 48,
+      posY: 165
+    },
+    largePitch: {
+      posX: 60,
+      posY: 220
+    }
+  },
+  {
+    num: 3,
+    smallPitch: {
+      posX: 102,
+      posY: 110
+    },
+    middlePitch: {
+      posX: 136,
+      posY: 165
+    },
+    largePitch: {
+      posX: 170,
+      posY: 220
+    }
+  },
+  {
+    num: 4,
+    smallPitch: {
+      posX: 168,
+      posY: 110
+    },
+    middlePitch: {
+      posX: 224,
+      posY: 165
+    },
+    largePitch: {
+      posX: 280,
+      posY: 220
+    }
+  },
+  {
+    num: 5,
+    smallPitch: {
+      posX: 234,
+      posY: 110
+    },
+    middlePitch: {
+      posX: 312,
+      posY: 165
+    },
+    largePitch: {
+      posX: 390,
+      posY: 220
+    }
+  },
+  {
+    num: 6,
+    smallPitch: {
+      posX: 201,
+      posY: 190
+    },
+    middlePitch: {
+      posX: 268,
+      posY: 285
+    },
+    largePitch: {
+      posX: 335,
+      posY: 350
+    }
+  },
+  {
+    num: 7,
+    smallPitch: {
+      posX: 69,
+      posY: 270
+    },
+    middlePitch: {
+      posX: 92,
+      posY: 405
+    },
+    largePitch: {
+      posX: 115,
+      posY: 480
+    }
+  },
+  {
+    num: 8,
+    smallPitch: {
+      posX: 69,
+      posY: 190
+    },
+    middlePitch: {
+      posX: 92,
+      posY: 285
+    },
+    largePitch: {
+      posX: 115,
+      posY: 350
+    }
+  },
+  {
+    num: 9,
+    smallPitch: {
+      posX: 135,
+      posY: 270
+    },
+    middlePitch: {
+      posX: 180,
+      posY: 405
+    },
+    largePitch: {
+      posX: 225,
+      posY: 480
+    }
+  },
+  {
+    num: 10,
+    smallPitch: {
+      posX: 135,
+      posY: 190
+    },
+    middlePitch: {
+      posX: 180,
+      posY: 285
+    },
+    largePitch: {
+      posX: 225,
+      posY: 350
+    }
+  },
+  {
+    num: 11,
+    smallPitch: {
+      posX: 201,
+      posY: 270
+    },
+    middlePitch: {
+      posX: 268,
+      posY: 405
+    },
+    largePitch: {
+      posX: 335,
+      posY: 480
+    }
+  }
+];
+
+export const fourTwoThreeOne = [
+  {
+    num: 1,
+    smallPitch: {
+      posX: 135,
+      posY: 30
+    },
+    middlePitch: {
+      posX: 180,
+      posY: 55
+    },
+    largePitch: {
+      posX: 225,
+      posY: 80
+    }
+  },
+  {
+    num: 2,
+    smallPitch: {
+      posX: 36,
+      posY: 90
+    },
+    middlePitch: {
+      posX: 48,
+      posY: 140
+    },
+    largePitch: {
+      posX: 60,
+      posY: 190
+    }
+  },
+  {
+    num: 3,
+    smallPitch: {
+      posX: 102,
+      posY: 90
+    },
+    middlePitch: {
+      posX: 136,
+      posY: 140
+    },
+    largePitch: {
+      posX: 170,
+      posY: 190
+    }
+  },
+  {
+    num: 4,
+    smallPitch: {
+      posX: 168,
+      posY: 90
+    },
+    middlePitch: {
+      posX: 224,
+      posY: 140
+    },
+    largePitch: {
+      posX: 280,
+      posY: 190
+    }
+  },
+  {
+    num: 5,
+    smallPitch: {
+      posX: 234,
+      posY: 90
+    },
+    middlePitch: {
+      posX: 312,
+      posY: 140
+    },
+    largePitch: {
+      posX: 390,
+      posY: 190
+    }
+  },
+  {
+    num: 6,
+    smallPitch: {
+      posX: 168,
+      posY: 150
+    },
+    middlePitch: {
+      posX: 224,
+      posY: 225
+    },
+    largePitch: {
+      posX: 280,
+      posY: 300
+    }
+  },
+  {
+    num: 7,
+    smallPitch: {
+      posX: 36,
+      posY: 210
+    },
+    middlePitch: {
+      posX: 48,
+      posY: 310
+    },
+    largePitch: {
+      posX: 60,
+      posY: 410
+    }
+  },
+  {
+    num: 8,
+    smallPitch: {
+      posX: 102,
+      posY: 150
+    },
+    middlePitch: {
+      posX: 136,
+      posY: 225
+    },
+    largePitch: {
+      posX: 170,
+      posY: 300
+    }
+  },
+  {
+    num: 9,
+    smallPitch: {
+      posX: 135,
+      posY: 270
+    },
+    middlePitch: {
+      posX: 180,
+      posY: 395
+    },
+    largePitch: {
+      posX: 225,
+      posY: 520
+    }
+  },
+  {
+    num: 10,
+    smallPitch: {
+      posX: 135,
+      posY: 210
+    },
+    middlePitch: {
+      posX: 180,
+      posY: 310
+    },
+    largePitch: {
+      posX: 225,
+      posY: 410
+    }
+  },
+  {
+    num: 11,
+    smallPitch: {
+      posX: 234,
+      posY: 210
+    },
+    middlePitch: {
+      posX: 312,
+      posY: 310
+    },
+    largePitch: {
+      posX: 390,
+      posY: 410
+    }
+  }
+];
